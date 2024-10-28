48dcc24eac51c94d17cc5aac857dc74fe859bb44
Dennis Antonio Landero Ramos <156869478+LanderoRamos@users.noreply.github.com>
2024-10-27 22:41:11 -0600

    Update MauiProgram.cs

--- Examen1parcial/MauiProgram.cs
+++ Examen1parcial/MauiProgram.cs
@@ -13,9 +13,7 @@ namespace Examen1parcial
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                })
-
-                .UseMauiMaps();
+                });
 
 #if DEBUG
             builder.Logging.AddDebug();
